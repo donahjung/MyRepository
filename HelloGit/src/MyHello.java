0ca22ef6b41895e1ccb68036fc77a5a23805fdc4
user <user@DESKTOP-JVEQEMM>
2021-06-21 23:31:26 +0900

    First commint in dev branch

--- HelloGit/src/MyHello.java
+++ HelloGit/src/MyHello.java
@@ -2,6 +2,8 @@
 public class MyHello {
 	public static void main(String args[]) {
 		System.out.println("Hello world");
+		System.out.println("Hello world2");
+		System.out.println("Hello world3");
 		System.out.println("this is my second class!");
 	}
 
