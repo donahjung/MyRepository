5ae77691149648d49f29777ccc811ec2985ae63a
user <user@DESKTOP-JVEQEMM>
2021-06-21 23:23:56 +0900

    add a new file

---
+++ HelloGit/src/MyHello.java
@@ -0,0 +1,11 @@
+
+public class MyHello {
+	public static void main(String args[]) {
+		System.out.println("Hello world");
+		System.out.println("this is my second class!");
+	}
+
+	public void sayHello() {
+
+	}
+}
